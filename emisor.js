aa334f88a473adda1f75f3cb11ec3debfe21e3d0
Leo Lorenzo <95876108+LeoL22002@users.noreply.github.com>
2023-11-12 14:32:42 -0400

    implementando alta disponibilidad

--- emisor.js
+++ emisor.js
@@ -1,40 +1,78 @@
-const amqp=require("amqplib");
-const rabbitConf={
-protoccol: 'amqp',
-hostname: 'localhost',
-port:5672,
-username:'leo22002',
-password:'root',
-vhost:'/',
-authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-}
-const cola="mensajes";
-const msgs=[
-    {"mensaje":"Wao"},
-    {"mensaje":"El que quiera perder su tiempo que me aconseje"},
-    {"mensaje":"Ivan deme lo 55 punto polfavol"},
-    {"mensaje":"El que ta quemao no folza"},
-    {"mensaje":"Ya sume to y yo paso en C"},
-    {"mensaje":"Aunque sea un 70 profe :("}
+const amqp = require('amqplib');
+
+const rabbitConf = [
+  {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5672,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL']
+  },
+  {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5673,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL']
+  }
 ];
+
+const cola = 'mensajes';
+const msgs = [
+  {"mensaje": "Wao"},
+  {"mensaje": "El que quiera perder su tiempo que me aconseje"},
+  {"mensaje": "Ivan deme lo 55 punto polfavol"},
+  {"mensaje": "El que ta quemao no folza"},
+  {"mensaje": "Ya sume to y yo paso en C"},
+  {"mensaje": "Aunque sea un 70 profe :("}
+];
+
 connect();
 
+async function connect() {
+  let connection;
+  let channel;
 
-async function connect(){
-try {
-    
-    const conn= await amqp.connect(rabbitConf);
-    console.log("Conexion Estbalecida");
-    const canal= await conn.createChannel();
-    console.log("Canal Creado");
-    const res= await canal.assertQueue(cola);
-    console.log("Cola Creada");
-    for (let msg in msgs) {
-        await canal.sendToQueue(cola,Buffer.from(JSON.stringify(msgs[msg])))
-        console.log("Mensaje Enviado");
+  for (const config of rabbitConf) {
+    try {
+      // Intenta conectar al nodo actual
+      connection = await amqp.connect(config);
+      console.log(`Conexión establecida con ${config.hostname}:${config.port}`);
+
+      // Si la conexión es exitosa, sal del bucle
+      break;
+    } catch (error) {
+      console.error(`Error al conectar a ${config.hostname}:${config.port}: ${error.message}`);
     }
+  }
 
-} catch (error) {
-    console.error(error);
-}    
+  if (!connection) {
+    console.error('No se pudo conectar a ninguno de los nodos.');
+    return;
+  }
+
+  try {
+    channel = await connection.createChannel();
+    console.log('Canal creado');
+
+    const result = await channel.assertQueue(cola);
+    console.log('Cola creada');
+
+    for (const msg of msgs) {
+      await channel.sendToQueue(cola, Buffer.from(JSON.stringify(msg)));
+      console.log('Mensaje enviado');
+    }
+  } catch (error) {
+    console.error('Error durante la operación con RabbitMQ:', error.message);
+  } 
+  // finally {
+  //   // Asegúrate de cerrar la conexión y el canal al finalizar
+  //  // if (channel) await channel.close();
+  //  // if (connection) await connection.close();
+  //  // console.log('Conexión cerrada');
+  // }
 }
--- receptor.js
+++ receptor.js
@@ -1,32 +1,75 @@
-const amqp=require("amqplib");
-const rabbitConf={
-protoccol: 'amqp',
-hostname: 'localhost',
-port:5672,
-username:'leo22002',
-password:'root',
-vhost:'/',
-authMechanism: ['PLAIN','AMQPLAIN','EXTERNAL']
-}
-const cola="mensajes";
+const amqp = require('amqplib');
+
+const rabbitConf = [
+  {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5672,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL']
+  },
+  {
+    protocol: 'amqp',
+    hostname: 'localhost',
+    port: 5673,
+    username: 'guest',
+    password: 'guest',
+    vhost: '/',
+    authMechanism: ['PLAIN', 'AMQPLAIN', 'EXTERNAL']
+  }
+];
+
+const cola = 'mensajes';
+
 connect();
 
+async function connect() {
+  let connection;
+  let channel;
 
-async function connect(){
-try {
-    
-    const conn= await amqp.connect(rabbitConf);
-    console.log("Conexion Establecida no bulto");
-    const canal= await conn.createChannel();
-    canal.consume(cola,mensaje=>{
-        let msj=JSON.parse(mensaje.content.toString());
-        console.log("Se ha recibido un mensaje:");
-        console.log(msj);
-        canal.ack(mensaje);
-        console.log("Se ha eliminado el mensaje.");
+  for (const config of rabbitConf) {
+    try {
+      // Intenta conectar al nodo actual
+      connection = await amqp.connect(config);
+      console.log(`Conexión establecida con ${config.hostname}:${config.port}`);
+
+      // Si la conexión es exitosa, sal del bucle
+      break;
+    } catch (error) {
+      console.error(`Error al conectar a ${config.hostname}:${config.port}: ${error.message}`);
+    }
+  }
+
+  if (!connection) {
+    console.error('No se pudo conectar a ninguno de los nodos.');
+    return;
+  }
+
+  try {
+    channel = await connection.createChannel();
+    console.log('Canal creado');
+
+        await channel.assertQueue(cola);
+
+    console.log('Esperando mensajes. Para salir, presiona CTRL+C');
+
+    // Configura el consumidor para recibir mensajes de la cola
+    channel.consume(cola, (mensaje) => {
+      if (mensaje !== null) {
+        let msj = JSON.parse(mensaje.content.toString());
+        
+        console.log("->",msj);
+        channel.ack(mensaje);
+        console.log('Se ha eliminado el mensaje.');
+      }
+      else{
+        console.log('Usted no tiene mensajes.');
+      }
     });
-
-} catch (error) {
-    console.error(error);
-}    
+  } catch (error) {
+    console.error('Error durante la operación con RabbitMQ:', error.message);
+  } 
+ 
 }
